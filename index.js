d2ff55ae59c2bea7daefe259108745a7e842ff15
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-29 13:48:35 +0300

    3.22: Lint configuration

--- index.js
+++ index.js
@@ -1,104 +1,104 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const Contact = require('./models/phonebook');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const Contact = require('./models/phonebook')
 
-app.use(express.json());
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(express.static('build'));
+app.use(express.json())
+morgan.token('body', req => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 app.get('/api/persons', (req, res, next) => {
-    Contact.find({})
-        .then(contacts => {
-            res.json(contacts);
-        })
-        .catch(error => next(error));
-});
+  Contact.find({})
+    .then(contacts => {
+      res.json(contacts)
+    })
+    .catch(error => next(error))
+})
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Contact.findById(req.params.id)
-        .then(contact => {
-            if (contact) {
-                res.json(contact);
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(error => next(error));
-});
+  Contact.findById(req.params.id)
+    .then(contact => {
+      if (contact) {
+        res.json(contact)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body;
+  const body = req.body
 
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'fields should not be empty',
-        });
-    }
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'fields should not be empty',
+    })
+  }
 
-    const contact = new Contact({
-        name: body.name,
-        number: body.number,
-    });
+  const contact = new Contact({
+    name: body.name,
+    number: body.number,
+  })
 
-    contact
-        .save()
-        .then(savedContact => res.json(savedContact))
-        .catch(error => next(error));
-});
+  contact
+    .save()
+    .then(savedContact => res.json(savedContact))
+    .catch(error => next(error))
+})
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body;
+  const body = req.body
 
-    const contact = {
-        name: body.name,
-        number: body.number,
-    };
+  const contact = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Contact.findByIdAndUpdate(req.params.id, contact, { new: true, runValidators: true, context: 'query' })
-        .then(updatedContact => {
-            res.json(updatedContact);
-        })
-        .catch(error => next(error));
-});
+  Contact.findByIdAndUpdate(req.params.id, contact, { new: true, runValidators: true, context: 'query' })
+    .then(updatedContact => {
+      res.json(updatedContact)
+    })
+    .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Contact.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end();
-        })
-        .catch(error => next(error));
-});
+  Contact.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 app.get('/info', (req, res, next) => {
-    const amount = Contact.countDocuments()
-        .then(count => {
-            const message = `
+  Contact.countDocuments()
+    .then(count => {
+      const message = `
             <p>Phonebook has info for ${count} people</p>
-            <p>${new Date()}</p>`;
-            res.send(message);
-        })
-        .catch(error => next(error));
-});
+            <p>${new Date()}</p>`
+      res.send(message)
+    })
+    .catch(error => next(error))
+})
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err.message);
+  console.error(err.message)
 
-    if (err.name === 'CastError') {
-        return res.status(400).json({ err: 'malformatted id' });
-    } else if (err.name === 'ValidationError') {
-        return res.status(400).json({ err: err.message });
-    }
+  if (err.name === 'CastError') {
+    return res.status(400).json({ err: 'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).json({ err: err.message })
+  }
 
-    next(err);
-};
+  next(err)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,44 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>');
-    process.exit(1);
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://test_user:${password}@cluster0.7ngz9.mongodb.net/phonebook?retryWrites=true&w=majority`;
+const url = `mongodb+srv://test_user:${password}@cluster0.7ngz9.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-});
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-});
+  name: String,
+  number: String,
+})
 
-const Contact = mongoose.model('Phonebook', contactSchema);
+const Contact = mongoose.model('Phonebook', contactSchema)
 
 if (process.argv.length === 3) {
-    console.log('phonebook:');
-    Contact.find({}).then(result => {
-        result.forEach(contact => {
-            console.log(`${contact.name} ${contact.number}`);
-        });
-        mongoose.connection.close();
-    });
+  console.log('phonebook:')
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(`${contact.name} ${contact.number}`)
+    })
+    mongoose.connection.close()
+  })
 } else {
-    const contact = new Contact({
-        name: process.argv[3],
-        number: process.argv[4],
-    });
+  const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
 
-    contact.save().then(result => {
-        console.log(`added ${result.name} number ${result.number} to phonebook`);
-        mongoose.connection.close();
-    });
+  contact.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
